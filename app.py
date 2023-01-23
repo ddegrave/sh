bf94f913f72defba011ffc20784b65e5f65e7fd7
ddegrave <denisdegrave123@gmail.com>
2023-01-23 22:32:15 +0100

    noatm

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from pythermalcomfort.models import pmv_ppd, a_pmv, set_tmp
+from pythermalcomfort.models import pmv_ppd, set_tmp
 
 app = Flask(__name__)
 app.secret_key = "abc"
@@ -51,10 +51,10 @@ def index():
         hdayproxy="no proximity use @ the moment"
 
     pmv_value=pmv_ppd(tdb, tr, vr, rh, met, clo,wme)
-    apmv_value=a_pmv(tdb, tr, vr, rh, met, clo, a_coefficient, wme)
+    
     sh_pmv=pmv_ppd(shtdb, shtr, vr, rh, met, clo,wme)
     set_value=set_tmp(tdb, tr, v, rh, met, clo, wme=0, body_surface_area=1.83, p_atm=101325, body_position='standing', units='SI', limit_inputs=True)
-    return render_template('index.html',kWhgain=kWhgain,hdayproxy=hdayproxy, centralheating=centralheating, proximityconso=proximityconso, sh_pmv=sh_pmv, set_value=set_value, pmv_value=pmv_value, apmv_value=apmv_value, tdb=tdb, tr=tr, vr=vr, rh=rh, met=met,clo=clo, a_coefficient=a_coefficient, ct=ct, cs=cs, cr=cr, pt=pt, ps=ps, pd=pd, c20=c20 )
+    return render_template('index.html',kWhgain=kWhgain,hdayproxy=hdayproxy, centralheating=centralheating, proximityconso=proximityconso, sh_pmv=sh_pmv, set_value=set_value, pmv_value=pmv_value, tdb=tdb, tr=tr, vr=vr, rh=rh, met=met,clo=clo, a_coefficient=a_coefficient, ct=ct, cs=cs, cr=cr, pt=pt, ps=ps, pd=pd, c20=c20 )
 
 if __name__ == '__main__':
     app.run(debug=True)
