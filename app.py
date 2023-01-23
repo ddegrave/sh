3574da9f7c8f084fe84271ba00ec9793c2855f77
ddegrave <denisdegrave123@gmail.com>
2023-01-23 17:45:22 +0100

    update

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask, render_template, request
-from pythermalcomfort.models import pmv
+from flask import Flask, render_template, request, jsonify, flash
+from pythermalcomfort.models import pmv_ppd
 
 app = Flask(__name__)
+app.secret_key = "abc"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        tdb = float(request.form['tdb'])
-        tr = float(request.form['tr'])
-        vr = float(request.form['vr'])
-        rh = float(request.form['rh'])
-        met = float(request.form['met'])
-        clo = float(request.form['clo'])
-        wme = 0
-        pmv_value = pmv(tdb, tr, vr, rh, met, clo,wme)
-        return render_template('index.html', pmv_value=pmv_value)
-    return render_template('index.html')
+    tdb = float(request.form['tdb'])
+    tr = float(request.form['tr'])
+    vr = float(request.form['vr'])
+    rh = float(request.form['rh'])
+    met = float(request.form['met'])
+    clo = float(request.form['clo'])
+    wme = 0
+    pmv_value=pmv_ppd(tdb, tr, vr, rh, met, clo,wme)
+    return render_template('index.html', pmv_value=pmv_value, tdb=tdb, tr=tr, vr=vr, rh=rh, met=met,clo=clo)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
